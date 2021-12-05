851b77eb9b07c9ebf80f2d381741bc2958c472c4
Vladimir Buinoff <buinoff@gmail.com>
2021-12-05 18:56:03 +0200

    Allocation of methods for working with sessions from the authorization class into a separate class and reduction of repeated code

--- app/core/Auth.php
+++ app/core/Auth.php
@@ -1,5 +1,4 @@
 <?php
-
     class Auth {
 
         public $logged_user = false;
@@ -8,17 +7,17 @@
         public function __construct() {
             $this->isAuth();
             if (isset($_GET['do']) && $_GET['do'] == 'login') $this->doLogin();
-            if (isset($_GET['do']) && $_GET['do'] == 'logout') $this->doLogout();
+            if (isset($_GET['do']) && $_GET['do'] == 'logout') Session::delete();
         }
 
-        public function doLogin() {
+        private function doLogin() {
             $message = false;
             if ($data = filter_input_array(INPUT_POST)) {
                 if ($user = $this->getUserByLogin($data['login'])) {
                     if (password_verify($data['password'], $user['pass_hash'])) {
                         // That's right, let's authorize the user and redirecting him to Home page.
                         $this->logged_user = $user;
-                        $this->writeSession();
+                        Session::writeSession($this->logged_user);
                         header('Location: ' . HOME);
                     } else {
                         $message['error']['message_heading'] = 'You enter a wrong password!';
@@ -35,7 +34,7 @@
             $this->errors = $message;
         }
 
-        public function getUserByLogin($login) { // Simple method for User Model in this case, works with arrays instead of objects
+        private function getUserByLogin($login) { // Simple method for User Model in this case, works with arrays instead of objects
             $users = require_once(ROOT . '/config/users.php');
             foreach ($users as $key => $user) {
                 if ($user['login'] == $login || $user['email'] == $login) {
@@ -45,11 +44,10 @@
             return false;
         }
 
-        public function isAuth() {
+        private function isAuth() {
             $message = false;
             if (isset($_SESSION["logged_user"])) { // If session variable logged_user exists
-                if ($this->checkSession()) {
-                    $this->logged_user = $this->getUserByLogin($_SESSION["logged_user"]);
+                if ($this->logged_user = Session::checkUserSession()) {
                     return true;
                 } else {
                     $message['error']['message_heading'] = 'Suspected XSS attack!';
@@ -58,47 +56,5 @@
                 }
             }
             return false; //The user is not logged in because variable logged_user not created or checking session is false
-        }
-
-        public function doLogout() {
-            session_regenerate_id();
-            setcookie(session_name(), session_id(), time()-3600);
-            unlink(session_save_path() . '/sess_' . $_SESSION[session_name()]);
-            unset($_COOKIE);
-            unset($_SESSION);
-            session_unset();
-            session_destroy();
-            header('Location: ' . HOME);
-        }
-
-        public function emergencyLogout() {
-            session_regenerate_id();
-            $compromised_cookies = $_COOKIE[session_name()];
-            unlink(session_save_path() . '/sess_' . $compromised_cookies);
-            setcookie(session_name(), session_id(), time()-3600);
-            unset($_COOKIE);
-            unset($_SESSION);
-            session_unset();
-            session_destroy();
-        }
-
-        public function writeSession() {
-            // Writing data to the session and cookie
-            $_SESSION['logged_user'] = $this->logged_user['login'];
-            $_SESSION[session_name()] = session_id();
-            $_SESSION['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
-            $_SESSION['USER_AGENT'] = $_SERVER['HTTP_USER_AGENT'];
-            setcookie(session_name(), session_id(), 1440, '/', HOME, false, true);
-            header('Location: ' . HOME);
-        }
-
-        public function checkSession() {
-            // Simple XSS attack check, prohibiting the same session from different user agent and from different IP
-            if (($_SERVER['REMOTE_ADDR'] === $_SESSION['REMOTE_ADDR']) && ($_SERVER['HTTP_USER_AGENT'] === $_SESSION['USER_AGENT']) && ($_SESSION[session_name()] === $_COOKIE[session_name()])) {
-                $this->logged_user = $_SESSION["logged_user"];
-                return true;
-            }
-            $this->emergencyLogout();
-            return false;
         }
     }
